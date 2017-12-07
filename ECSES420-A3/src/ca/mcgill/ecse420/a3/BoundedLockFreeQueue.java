3fb5214f2c215977a98c286c8289ffdd8d1460b8
sebastien177 <sebastien.arrese@gmail.com>
2017-12-07 14:53:54 -0500

    minor changes

--- ECSES420-A3/src/ca/mcgill/ecse420/a3/BoundedLockFreeQueue.java
+++ ECSES420-A3/src/ca/mcgill/ecse420/a3/BoundedLockFreeQueue.java
@@ -7,30 +7,30 @@ public class BoundedLockFreeQueue<T> {
 
     private AtomicInteger head = new AtomicInteger(0);
     private AtomicInteger tail = new AtomicInteger(0);
-    private AtomicInteger tailCommit = new AtomicInteger(0);
-    private AtomicInteger latentCapacity;
+    private AtomicInteger tailValue = new AtomicInteger(0);
+    private AtomicInteger remainingSpace;
 
     public BoundedLockFreeQueue(int capacity) {
         items = (T[]) new Object[capacity];
-        latentCapacity = new AtomicInteger(capacity);
+        remainingSpace = new AtomicInteger(capacity);
     }
 
     public void enqueue(T item) throws InterruptedException {
-        int lc = latentCapacity.get();
-        while (lc <= 0 || !latentCapacity.compareAndSet(lc, lc - 1)) 
-            lc = latentCapacity.get();
+        int rS = remainingSpace.get();
+        while (rS <= 0 || !remainingSpace.compareAndSet(rS, rS - 1)) 
+        	rS = remainingSpace.get();
         
         int t = tail.getAndIncrement();
         items[t % items.length] = item;
 
-        while (tailCommit.compareAndSet(t, t + 1)) { };
+        while (tailValue.compareAndSet(t, t + 1)) { };
     }
 
     public T dequeue() throws InterruptedException {
         int h = head.getAndIncrement();
-        while (h >= tailCommit.get()) { };
+        while (h >= tailValue.get()) { };
         T item = items[h % items.length];
-        latentCapacity.incrementAndGet();
+        remainingSpace.incrementAndGet();
 
         return item;
     }
